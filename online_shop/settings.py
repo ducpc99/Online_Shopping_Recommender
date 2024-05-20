f69b88b95cf5948076e318b6ef11b90ac9561f6e
ducpc99 <22022607@vnu.edu.vn>
2024-05-20 21:49:17 +0700

    update_database

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -73,8 +73,11 @@ WSGI_APPLICATION = 'online_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_OS',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
